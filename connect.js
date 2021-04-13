77b47285926cc36ad165f64ea455d00e96b0f6f7
Fran Swan <71414784+franswan@users.noreply.github.com>
2021-04-13 02:39:25 -0700

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,5 +1,16 @@
 "use strict";
 
+
+const { activate } = useEthers();
+
+  useEffect(() => {
+    injected.isAuthorized().then((isAuthorized) => {
+      if (isAuthorized) {
+        activate(injected);
+      }
+    });
+  }, [activate]); 
+
 /**
  * puppr 
  */
